a788f27dd0b41ef0022fd147eadc08bcc55b5715
PSchwan <paul.schwan@hotmail.com>
2017-06-30 10:03:10 +0100

    Fixing the setup issues, so that Spring can find the view.  Very basic view setup now

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # schwan_kalah
+
+
+
+initial Spring boot project / directory layout based on: https://www.mkyong.com/spring-boot/spring-boot-hello-world-example-jsp/
--- src/main/java/com/schwan/kalah/KalahController.java
+++ src/main/java/com/schwan/kalah/KalahController.java
@@ -1,21 +1,28 @@
 package com.schwan.kalah;
 
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class KalahController {
 
-    // inject via application.properties
-    //@Value("${welcome.message:test}")
+    @Value("${welcome.message:test}")
     private String message = "Hello World";
 
-    @RequestMapping("/test")
+    @RequestMapping("/welcome")
     public String welcome(Map<String, Object> model) {
-        System.out.println("hello world");
+        System.out.println("hello world, " + this.message);
         model.put("message", this.message);
         return "welcome";
+
+
+        /*ModelAndView modelAndView = new ModelAndView("welcome");
+        modelAndView.addObject("message", this.message);
+        return modelAndView;*/
     }
 
 }
